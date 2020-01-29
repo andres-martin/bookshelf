4f58c791ab0d096907d1bdf52a03f21ddbb87c64
Andres Martin Peña Rivera <apena@BLX-CO-apena.local>
2020-01-29 14:33:42 -0500

    [add] resolve method to delete a record

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -14,15 +14,23 @@ module Types
     # end
 
     field :create_author, Types::AuthorType, mutation: Mutations::CreateAuthor
+
     field :update_author, Boolean, null: false, description: "Update an author" do
       argument :author, Types::AuthorInputType, required: true
     end
 
     def update_author(author:)
       existing = Author.where(id: author[:id]).first
-
       existing&.update_attributes author.to_h
     end
 
+    field :delete_author, Boolean, null: false, description: "Delete an author" do
+      argument :id, ID, required: true
+    end
+
+    def delete_author(id:)
+      Author.where(id: id).destroy_all
+      true
+    end
   end
 end
